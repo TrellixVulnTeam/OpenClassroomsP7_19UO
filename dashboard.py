c01c874e39b9af1e64325313784dda9258018929
Borderon Julien <borderonjulien@gmail.com>
2022-04-13 10:07:37 +0200

    delete toolbox

--- dashboard.py
+++ dashboard.py
@@ -58,6 +58,7 @@ def main():
     predict_btn = st.button('Prédire')
     if predict_btn:
         data = df[df['SK_ID_CURR']==int(id)].to_numpy().tolist()
+        st.write(int(id))
         del data[0]
         st.write(data)
         pred = request_prediction(API_url,data)[0]
