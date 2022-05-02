d5e7a1da18880f12da6543991195c355cc99bed1
Borderon Julien <borderonjulien@gmail.com>
2022-05-02 16:46:41 +0200

    définition de fonctions utiles amélioration du fichier csv

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pickle
 from fastapi import FastAPI
 import re
@@ -31,35 +30,4 @@ def predict(data):
 
 if __name__ == '__main__':
    uvicorn.run(app, host='127.0.0.1', port=8000)
-=======
-import pickle
-from fastapi import FastAPI
-import re
-import joblib
-import numpy as np
-import ast
 
-
-app = FastAPI()
-
-
-model = joblib.load(open("model.pkl", "rb"))
-
-@app.get("/")
-async def root():
-   return {"message": "Hello World"}
-
-@app.get("/predict/")
-def predict(data):
-    data = data.replace("(","[[")
-    data =data.replace(")","]]")
-    data = ast.literal_eval(data)
-    prediction = model.predict(data)
-    return {
-       'prediction': prediction[0],
-   }
-
-
-if __name__ == '__main__':
-   uvicorn.run(app, host='127.0.0.1', port=8000)
->>>>>>> 413a90f8df01d268d6360bdbc6c6327d92eb62e5
