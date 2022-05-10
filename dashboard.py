72ea6b0a8f3068523f378924e9f8ddea2ac8533a
Borderon Julien <borderonjulien@gmail.com>
2022-05-10 11:38:17 +0200

    test

--- dashboard.py
+++ dashboard.py
@@ -23,7 +23,7 @@ def graph(dataframe,feature,id,df) :
     fig = plt.figure(figsize=(10, 4))
     sns.histplot(data = dataframe , x = feature, hue = 'TARGET', stat = 'density', kde=True, common_norm=False)
     #plt.axvline(df[df['SK_ID_CURR']==id][feature].item(),0,2, color ='black', label='client')
-    ax_hist.axvline(df[df['SK_ID_CURR']==id][feature].item(), color='orange', linestyle='--',label='Client')
+    fig.axvline(df[df['SK_ID_CURR']==id][feature].item(), color='orange', linestyle='--',label='Client')
     st.write("Valeur de la feature",feature,"pour le client :", df[df['SK_ID_CURR']==id][feature].item())
     if feature=='AMT_INCOME_TOTAL' :
         plt.xlim(0,0.5E6)
