d9814e8143f35a5a40687fff78686900123c18d4
Borderon Julien <borderonjulien@gmail.com>
2022-05-19 11:10:12 +0200

    test titre graphique latex

--- dashboard.py
+++ dashboard.py
@@ -99,8 +99,8 @@ def main():
                 if 'EXT_SOURCE' in client_infos :
                     graph(df_train,'EXT_SOURCE_3',id,df_to_predict_display)
                     st.latex(r'''
-                    \underline{Courbes} représentant les distributions des revenus ext\érieurs de type 3 ({EXT}_{SOURCE}_{3}) 
-                             pour les dossiers validés (courbe bleue) et non validés (courbe orange)''')
+                         \underline{a + ar}
+                        ''')
                     graph(df_train,'EXT_SOURCE_2',id,df_to_predict_display)
                     st.latex(r'''
                          \underline{a + ar + a r^2 + a r^3 + \cdots + a r^{n-1}} =
